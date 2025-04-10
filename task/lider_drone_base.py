084c1145445df48b2b2d50d9ea1d3365ca686736
luckydipper <qhrqufdlek@khu.ac.kr>
2025-04-10 13:55:14 +0900

    fix: lidar destination  coordinate error
    add translation term in detination area

--- task/lider_drone_base.py
+++ task/lider_drone_base.py
@@ -9,27 +9,27 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-    def laycast(self, position:np.ndarray, quaternion:np.ndarray, lidar_reach: float) -> np.ndarray:
+    def laycast(self, position:np.ndarray, quaternion:np.ndarray, lidar_reach: float, num_ray: int) -> np.ndarray:
         # You need to change this term to change lidar observation.
-        NUM_RAY = 4 
         R = p.getMatrixFromQuaternion(quaternion)
         R = np.array(R).reshape(3,3)
-        ray_from = [position for _ in range(NUM_RAY)]
-        ray_to = [ R@ np.array([
-                        lidar_reach * math.sin(2. * math.pi * float(d_theta) / NUM_RAY),
-                        lidar_reach * math.cos(2. * math.pi * float(d_theta) / NUM_RAY),
-                        1
-                        ])
-                  for d_theta in range(NUM_RAY)]
+        ray_from = [position for _ in range(num_ray)]
+        ray_to = [ position + R @ np.array([
+                                lidar_reach * math.sin(2. * math.pi * float(d_theta) / num_ray),
+                                lidar_reach * math.cos(2. * math.pi * float(d_theta) / num_ray),
+                                0
+                                ])
+                                for d_theta in range(num_ray)]
         
         # code for visualize
         line_color = [0, 1, 0]
-        for i in range(NUM_RAY):
+        for i in range(num_ray):
             p.addUserDebugLine(ray_from[i], ray_to[i], line_color)
         # ---
         
+        NUM_THREAD = 1
         distances = [normalized_dist* lidar_reach 
-                     for ray_id, body_id, normalized_dist, xyz, direction in p.rayTestBatch(ray_from,ray_to,4)]
+                     for ray_id, body_id, normalized_dist, xyz, direction in p.rayTestBatch(ray_from,ray_to,NUM_THREAD)]
         return np.array(distances)
     
     def compute_observation_by_id(self, agent_id: int) -> np.ndarray:
@@ -44,6 +44,8 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
         ang_vel, ang_pos, lin_vel, lin_pos, quaternion = raw_state
 
         # depending on angle representation, return the relevant thing
+
+        lidar_length = 4; num_ray = 3
         if self.angle_representation == 0:
             return np.concatenate(
                 [
@@ -51,7 +53,7 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
                     ang_pos,
                     lin_vel,
                     lin_pos,
-                    self.laycast(lin_pos, quaternion,4),
+                    self.laycast(lin_pos, quaternion, lidar_length, num_ray),
                     self.past_actions[agent_id],
                     self.start_pos[agent_id],
                 ],
@@ -64,7 +66,7 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
                     quaternion,
                     lin_vel,
                     lin_pos,
-                    self.laycast(lin_pos, quaternion,4),
+                    self.laycast(lin_pos, quaternion, lidar_length, num_ray),
                     self.past_actions[agent_id],
                     self.start_pos[agent_id],
                 ],
