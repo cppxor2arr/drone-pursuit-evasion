2b669dfa066ddbb1e3b45491da021c4dab3a743d
cppxor2arr <22780237+cppxor2arr@users.noreply.github.com>
2025-04-24 20:08:33 +0900

    style: format code

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from task.lider_drone_base import LidarDroneBaseEnv
 import pybullet as p
 from PyFlyt.pz_envs import MAQuadXHoverEnvV2
 import numpy as np
-env = LidarDroneBaseEnv(lidar_reach=4., num_ray=3, render_mode="human", flight_mode=7, start_pos=np.array([[1,1,1], [-1,-1,1]]),start_orn=np.array([[0,0,0], [0,0,0]]))#MAQuadXHoverEnvV2(render_mode="human")
+
+env = LidarDroneBaseEnv(
+    lidar_reach=4.0,
+    num_ray=3,
+    render_mode="human",
+    flight_mode=7,
+    start_pos=np.array([[1, 1, 1], [-1, -1, 1]]),
+    start_orn=np.array([[0, 0, 0], [0, 0, 0]]),
+)  # MAQuadXHoverEnvV2(render_mode="human")
 observations, infos = env.reset()
 
 while env.agents:
     # this is where you would insert your policy
     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
-    #actions = {agent: [0,0,0,0] for agent in env.agents}
-    observations, rewards, terminations, truncations, infos = env.step_pursuit_evade(actions)
+    # actions = {agent: [0,0,0,0] for agent in env.agents}
+    observations, rewards, terminations, truncations, infos = env.step_pursuit_evade(
+        actions
+    )
     print(observations)
     print(actions)
     # for agent in env.agents:
--- task/lider_drone_base.py
+++ task/lider_drone_base.py
@@ -2,11 +2,14 @@ from gymnasium import spaces
 import numpy as np
 from typing import Any, Literal
 import pybullet as p
-from PyFlyt.pz_envs.quadx_envs.ma_quadx_hover_env import MAQuadXHoverEnv #MAQuadXBaseEnv
+from PyFlyt.pz_envs.quadx_envs.ma_quadx_hover_env import (
+    MAQuadXHoverEnv,
+)  # MAQuadXBaseEnv
 import math
 
+
 class Actions:
-    def __init__(self, length: float = 1.):
+    def __init__(self, length: float = 1.0):
         self.norm = 1 / np.sqrt(3)
         self.length = length
         self.vectors = [
@@ -35,60 +38,80 @@ class Actions:
     def __iter__(self):
         return (v * self.length for v in self.vectors)
 
+
 class LidarDroneBaseEnv(MAQuadXHoverEnv):
-    def __init__(self, lidar_reach: float, num_ray: int, angle_representations:str = "quaternion", *args, **kwargs):
+    def __init__(
+        self,
+        lidar_reach: float,
+        num_ray: int,
+        angle_representations: str = "quaternion",
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
-    
+
         self.lidar_reach = lidar_reach
         self.num_ray = num_ray
 
         translation_dim = velocity_dim = 3
         target_position = 3
         self._observation_space = spaces.Box(
-                    low=-np.inf,
-                    high=np.inf,
-                    shape=(translation_dim + velocity_dim + target_position + self.num_ray, ),
-                    dtype=np.float64,
-                )
+            low=-np.inf,
+            high=np.inf,
+            shape=(translation_dim + velocity_dim + target_position + self.num_ray,),
+            dtype=np.float64,
+        )
         self.actions = Actions(self.lidar_reach)
         self._action_space = spaces.Discrete(len(self.actions))
 
-    def laycast(self, position:np.ndarray, quaternion:np.ndarray) -> np.ndarray:
+    def laycast(self, position: np.ndarray, quaternion: np.ndarray) -> np.ndarray:
         # You need to change this term to change lidar observation.
         R = p.getMatrixFromQuaternion(quaternion)
-        R = np.array(R).reshape(3,3)
+        R = np.array(R).reshape(3, 3)
         ray_from = [position for _ in range(self.num_ray)]
-        ray_to = [ position + R @ np.array([
-                                self.lidar_reach * math.sin(2. * math.pi * float(d_theta) / self.num_ray),
-                                self.lidar_reach * math.cos(2. * math.pi * float(d_theta) / self.num_ray),
-                                0
-                                ])
-                                for d_theta in range(self.num_ray)]
-        
+        ray_to = [
+            position
+            + R
+            @ np.array(
+                [
+                    self.lidar_reach
+                    * math.sin(2.0 * math.pi * float(d_theta) / self.num_ray),
+                    self.lidar_reach
+                    * math.cos(2.0 * math.pi * float(d_theta) / self.num_ray),
+                    0,
+                ]
+            )
+            for d_theta in range(self.num_ray)
+        ]
+
         # code for visualize
         # line_color = [0, 1, 0]
         # for i in range(self.num_ray):
         #     p.addUserDebugLine(ray_from[i], ray_to[i], line_color)
         # ---
-        
+
         NUM_THREAD = 1
-        distances = [normalized_dist* self.lidar_reach 
-                     for ray_id, body_id, normalized_dist, xyz, direction in p.rayTestBatch(ray_from,ray_to,NUM_THREAD)]
+        distances = [
+            normalized_dist * self.lidar_reach
+            for ray_id, body_id, normalized_dist, xyz, direction in p.rayTestBatch(
+                ray_from, ray_to, NUM_THREAD
+            )
+        ]
         return np.array(distances)
-    
+
     def compute_observation_by_id(self, agent_id: int) -> np.ndarray:
         raw_state = self.compute_attitude_by_id(agent_id)
 
         ang_vel, ang_pos, lin_vel, lin_pos, quaternion = raw_state
 
         return np.concatenate(
-                [
-                    lin_pos,
-                    lin_vel,
-                    self.laycast(lin_pos, quaternion),
-                ],
-                axis=-1,
-            )
+            [
+                lin_pos,
+                lin_vel,
+                self.laycast(lin_pos, quaternion),
+            ],
+            axis=-1,
+        )
 
     def compute_term_trunc_reward_info_by_id(
         self, agent_id: int
@@ -127,18 +150,22 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
 
         return term, trunc, reward, info
 
-    def step(self, actions: dict[str, int]) -> tuple[
+    def step(
+        self, actions: dict[str, int]
+    ) -> tuple[
         dict[str, Any],
         dict[str, float],
         dict[str, bool],
         dict[str, bool],
         dict[str, dict[str, Any]],
     ]:
-        #TODO we need to consider single agent step fucntion
-        assert(True)
-        return 
-    
-    def step_pursuit_evade(self, actions: dict[str, int])  -> tuple[
+        # TODO we need to consider single agent step fucntion
+        assert True
+        return
+
+    def step_pursuit_evade(
+        self, actions: dict[str, int]
+    ) -> tuple[
         dict[str, Any],
         dict[str, float],
         dict[str, bool],
@@ -146,12 +173,12 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
         dict[str, dict[str, Any]],
     ]:
         NUM_AGENT = len(actions.items())
-        assert(NUM_AGENT == 2)
+        assert NUM_AGENT == 2
         for k, v in actions.items():
             agent_idx = self.agent_name_mapping[k]
-            target_idx = int((agent_idx + 1)%NUM_AGENT)
-            x,y,z = self.compute_observation_by_id(target_idx)[:3]
-            dxdydz = self.actions[v] 
+            target_idx = int((agent_idx + 1) % NUM_AGENT)
+            x, y, z = self.compute_observation_by_id(target_idx)[:3]
+            dxdydz = self.actions[v]
 
             next_x = x + dxdydz[0]
             next_y = y + dxdydz[1]
@@ -185,10 +212,11 @@ class LidarDroneBaseEnv(MAQuadXHoverEnv):
                 infos[ag].update(info)
 
                 # compute observations
-                observations[ag] = np.concatenate([
-                    self.compute_observation_by_id(ag_id),
-                    self.compute_observation_by_id( (ag_id+1) % NUM_AGENT)[:3] 
-                ] # append pursuit and observation target
+                observations[ag] = np.concatenate(
+                    [
+                        self.compute_observation_by_id(ag_id),
+                        self.compute_observation_by_id((ag_id + 1) % NUM_AGENT)[:3],
+                    ]  # append pursuit and observation target
                 )
         # increment step count and cull dead agents for the next round
         self.step_count += 1
