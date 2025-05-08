a711602f7334adf86e85248c6a7fe742280e29e8
Shihyun Park <22780237+cppxor2arr@users.noreply.github.com>
2025-05-08 16:54:11 +0900

    feat: Visualize the flight dome
    Visualize the flight dome of the environment to show that collision and exceeding the boundaries are the same.

--- main.py
+++ main.py
@@ -3,6 +3,39 @@ import pybullet as p
 from PyFlyt.pz_envs import MAQuadXHoverEnvV2
 import numpy as np
 
+
+def render_flight_dome(env):
+    concaveSphereVisualId = env.aviary.createVisualShape(
+        shapeType=p.GEOM_MESH,
+        fileName="hi_res_sphere.obj",
+        meshScale=[-env.flight_dome_size] * 3,
+        rgbaColor=[1.0, 0.0, 0.0, 0.8],
+        specularColor=[0.4, 0.4, 0.4],
+    )
+    concaveSphereId = env.aviary.createMultiBody(
+        baseMass=0,
+        baseVisualShapeIndex=concaveSphereVisualId,
+        basePosition=[0.0, 0.0, 0.0],
+        useMaximalCoordinates=True,
+        flags=0,
+    )
+    convexSphereVisualId = env.aviary.createVisualShape(
+        shapeType=p.GEOM_MESH,
+        fileName="hi_res_sphere.obj",
+        meshScale=[env.flight_dome_size] * 3,
+        rgbaColor=[1.0, 0.0, 0.0, 0.8],
+        specularColor=[0.4, 0.4, 0.4],
+    )
+    convexSphereId = env.aviary.createMultiBody(
+        baseMass=0,
+        baseVisualShapeIndex=convexSphereVisualId,
+        basePosition=[0.0, 0.0, 0.0],
+        useMaximalCoordinates=True,
+        flags=0,
+    )
+    env.aviary.register_all_new_bodies()
+
+
 env = LidarDroneBaseEnv(
     lidar_reach=4.0,
     num_ray=3,
@@ -11,8 +44,11 @@ env = LidarDroneBaseEnv(
     start_pos=np.array([[1, 1, 1], [-1, -1, 1]]),
     start_orn=np.array([[0, 0, 0], [0, 0, 0]]),
 )  # MAQuadXHoverEnvV2(render_mode="human")
+
 observations, infos = env.reset()
 print(observations)
+
+render_flight_dome(env)
 
 while env.agents:
     # this is where you would insert your policy
